452b7b0470798d86403cad9c3b829443fb187f96
Nikant Yadav <nikant.iaastha@gmail.com>
2026-02-07 23:50:27 +0530

    Frontend

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,24 +1,23 @@
-const express = required("express");
+const express = require("express"); // Fixed
 const multer = require("multer");
 const router = express.Router();
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
     },
-    filename: (req,file, cb) => {
+    filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
-router.post("/upload", 
-    upload.single("file"), (req, res) => {
-        res.json({
-            message: "File Uploaded Sucess",
-            file : req.file,
-        });
-    }
-);
+router.post("/upload", upload.single("file"), (req, res) => {
+    res.json({
+        message: "File Uploaded Success",
+        file: req.file,
+    });
+});
 
-module.exports = routes;
+module.exports = router; // Fixed
